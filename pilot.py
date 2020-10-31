30cd66accf15dcabba0f1f8035b37ae63bba0c9e
oOHiyoriOo@Parrot <wolf.sf75@googlemail.com>
2020-10-31 23:29:57 +0100

    more tests

--- pilot.py
+++ pilot.py
@@ -1,5 +1,6 @@
 import filecmp
 import os
+import json
 
 from flask import Flask, request
 from flask_restful import Resource, Api
@@ -19,11 +20,12 @@ api = Api(app)
 
 class webhook(Resource):
     def post(self):
-        print(request.data,end="===\nDATA\n===\n")
-        print(request.form,end="===\nFORM\n===\n")
+        data = str(request.data)[2:len(str(request.data)) -1]
+        data = json.loads(data)
+        print(data['repository']['name'] )
         return 200
 
 api.add_resource(webhook, '/github.push')
 
 if __name__ == '__main__':
-    app.run(debug=True,port=8080)
+    app.run(debug=False,host='0.0.0.0',port=8080)
