cbaf47cee423385eb9ebf5d88867fa72c21ac9f0
oOHiyoriOo@Parrot <wolf.sf75@googlemail.com>
2020-11-01 09:22:51 +0100

    added bot version

--- gameQuery.py
+++ gameQuery.py
@@ -1,13 +1,23 @@
 import discord
+import asyncio
 from ZeroLogger.ZeroLogger import *
 import config
 
-client = discord.Client()
 
+botVersion = 0.1
+
+intents = discord.Intents(messages=True)
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
     logo(["==============================","Bot is Ready","=============================="])
+    # Setting `Playing ` status
+    await client.change_presence(activity=discord.Game(name="Running on version: "+str(botVersion)))
+
+
+
 
 @client.event
 async def on_message(msg):
