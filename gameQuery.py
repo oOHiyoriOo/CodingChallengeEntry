536783ab93dc419bfd9ada68cb562c6cd8f2a43c
Strelizia <sidney.marvin.fricke@gmail.com>
2020-11-02 08:58:58 +0100

    fixxed boot and starting to make the game now.

--- gameQuery.py
+++ gameQuery.py
@@ -3,14 +3,17 @@ import asyncio
 from ZeroLogger.ZeroLogger import *
 import config
 import random
+from tinydb import TinyDB, Query
 
 botVersion = 0.2
+
+query = Query()
+Players = TinyDB('player.json')
 
 intents = discord.Intents(messages=True,guilds=True)
 
 client = discord.Client(intents=intents)
 
-Players = []
 Running = False
 Searching = False
 
@@ -19,14 +22,14 @@ async def on_ready():
     logo(["==============================","Bot is Ready","=============================="])
     # Setting `Playing ` status
     await client.change_presence(activity=discord.Game(name="Running on version: "+str(botVersion)))
-    asyncio.sleep(5)
+    await asyncio.sleep(5)
     await client.change_presence(activity=discord.Game(name="THE Game. "+random.choice([":3",":)",";3","<3"] ) ) )
 
 @client.event
 async def on_message(msg):
     await client.wait_until_ready()
     if msg.channel.id == 772484220253503509:
-        global Players
+
         global Searching
 
         
@@ -34,10 +37,11 @@ async def on_message(msg):
             Players.append(msg.author.id)
             omsg = await msg.channel.send("Starting a game, everyone has 60 sec. to join!")
             Searching = True
-            client.loop.create_task(GameLobby(omsg)) 
+            await GameLobby(omsg)
 
         elif msg.content == "*join" and not Running and Searching:
-            Players.append(msg.author.id)
+            if not msg.author.id in Players:
+                Players.append(msg.author.id)
 
 
 
@@ -50,8 +54,12 @@ async def GameLobby(omsg):
         await omsg.edit(content="Starting a game, everyone has "+str(60 -i)+" sec. to join!")
         await asyncio.sleep(1)
 
+    await omsg.edit(content="Game Starts!")
     Searching = False
     Running = True
     
+    for Player in Players.search(query.user >= 0):
+        print(Player)
+
 
 client.run(config.token)
